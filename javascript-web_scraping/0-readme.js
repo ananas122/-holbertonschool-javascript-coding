e7fcd166cc96533f5092b87a732368ab6b6e580b
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-18 01:14:48 +0200

    Try with #!/usr/local/bin/node

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,2 @@
 #!/usr/local/bin/node
-const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, contents) {
-  if (contents === undefined) {
-    console.log(err);
-  } else {
-    console.log(contents);
-  }
-});
+
