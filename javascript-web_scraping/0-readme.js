2b0f8df2e456a5e4e338d54d2dd85c14f529b1ac
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-18 01:08:29 +0200

    Try

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,2 +1,9 @@
-#!/opt/homebrew/bin/node
-
+#!/usr/local/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, contents) {
+  if (contents === undefined) {
+    console.log(err);
+  } else {
+    console.log(contents);
+  }
+});
