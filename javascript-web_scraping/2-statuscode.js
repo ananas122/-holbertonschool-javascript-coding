d899a1189f8b3c40a041060aa5484a8cad22afaf
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-18 23:00:58 +0200

    Requete get

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/opt/homebrew/bin/node
+
+const request = require('request');
+
+// Récupération de l'URL depuis le 1er argv en ligne de commande.
+const url = process.argv[2];
+
+// Faire la requête GET.
+request.get(url, (error, response) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    // Afficher le code de statut.
+    console.log('code:', response.statusCode);
+  }
+});
