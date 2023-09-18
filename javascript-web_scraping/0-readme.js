07b65261e225c6aff4644ee5d2e0258a93f2edbf
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-18 14:42:32 +0200

    Script that reads and prints the content file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+// Replace 'your-file.txt' with the path to the file you want to read
+const filePath = 'cisfun.txt';
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+  console.log('File content:');
+  console.log(data);
+});
