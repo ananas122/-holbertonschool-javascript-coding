9d1c49c9d62cd54e0f640ba518b1e4bb4f1a7ec1
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-18 14:56:27 +0200

    Correction error

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
+// module file system
 const fs = require('fs');
 
-// Replace 'your-file.txt' with the path to the file you want to read
 const filePath = 'cisfun.txt';
 
 fs.readFile(filePath, 'utf8', (err, data) => {
@@ -9,6 +9,5 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     console.error('Error reading the file:', err);
     return;
   }
-  console.log('File content:');
   console.log(data);
 });
