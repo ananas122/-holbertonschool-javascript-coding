3e032f19b72f2c9ecb6d90a9b6e2f61ccc9c91ef
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-18 15:12:39 +0200

    Correction with arg[2]

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,4 +1,4 @@
-#!/opt/homebrew/bin/node
+#!/usr/bin/node
 // process.argv[2]' est le 3eme arg : contient le chemin du fichier à lire.
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
