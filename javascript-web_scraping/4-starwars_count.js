3d9b6603f97a140ebdf8c157628bcc20286ee8a8
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-19 11:45:25 +0200

    Changement variable

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/opt/homebrew/bin/node
 const request = require('request');
 let countFilms = 0;
 
@@ -6,13 +6,13 @@ request(process.argv[2], function (err, _response, body) {
   if (err == null) {
     const resp = JSON.parse(body); // Analyse la resp JSON en un objet JS
 
-    const {results} = resp;// Obtient la propriété 'results' de l'objet 'resp'.
+    const { results } = resp;// Obtient la propriété 'results' de l'objet 'resp'.
 
-    for (const _personnage of results) {
-      const {characters} = element;
+    for (const element of results) {
+      const { characters } = element;
 
       for (const personnage of characters) {
-        if (personnage.search('18') > 0) { // Vérifie si le 'personnage' contient '18'.
+        if (personnage.search('18') >= 0) {
           countFilms++;
         }
       }
