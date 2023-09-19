4330376fd35837b414c7653848e7bd5aeaa5b5dd
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-19 11:37:12 +0200

    Task completed

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,4 @@
-#!/opt/homebrew/bin/node
-
+#!/usr/bin/node
 const request = require('request');
 
 const apiUrl = process.argv[2];
@@ -7,13 +6,24 @@ const apiUrl = process.argv[2];
 request(apiUrl, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const todo = JSON.parse(body);
+
     const completedTasksByUser = {};
 
-    todo.forEach((todo) => {
-      if (todo.completed) {
-        completedTasksByUser[todo.userId] = (completedTasksByUser[todo.userId] || 0) + 1;
+    // Parcourt les tâches dans la réponse JSON.
+    for (const task of todo) {
+      // Vérifie si la tâche est complétée.
+      if (task.completed) {
+        // Obtient l'ID de l'utilisateur de la tâche.
+        const userId = task.userId;
+
+        if (completedTasksByUser[userId]) {
+          completedTasksByUser[userId]++;
+        } else {
+        // user a accompli sa 1er tâche complétée.
+          completedTasksByUser[userId] = 1;
+        }
       }
-    });
+    }
 
     console.log(completedTasksByUser);
   } else {
