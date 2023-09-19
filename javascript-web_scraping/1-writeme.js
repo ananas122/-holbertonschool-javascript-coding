0e78abbccfaf3460f0d2c08dd4c35df833457e23
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-19 09:49:16 +0200

    Add comment

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+// fs = module pr travailler avc des fichiers
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err, data) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
     console.error(err);
   }
