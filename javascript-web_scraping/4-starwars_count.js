54a366aa5d69095f63f1301a383ebcc5ea20d237
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-19 11:22:44 +0200

    Search personnage Correction

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,10 +6,10 @@ request(process.argv[2], function (err, _response, body) {
   if (err == null) {
     const resp = JSON.parse(body); // Analyse la resp JSON en un objet JS
 
-    const results = resp.results;// Obtient la propriété 'results' de l'objet 'resp'.
+    const {results} = resp;// Obtient la propriété 'results' de l'objet 'resp'.
 
-    for (const personnage of results) {
-      const characters = element.characters;
+    for (const _personnage of results) {
+      const {characters} = element;
 
       for (const personnage of characters) {
         if (personnage.search('18') > 0) { // Vérifie si le 'personnage' contient '18'.
