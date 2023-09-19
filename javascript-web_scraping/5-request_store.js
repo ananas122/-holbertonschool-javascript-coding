254983ebd922a8cc7d8e93818c93e3baa7b5c740
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-09-19 11:16:47 +0200

    Search personnage

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const fs = require('fs');// module pr gerer les operations de fichier
+const fs = require('fs');
 const apiURL = process.argv[2];
 const filePath = process.argv[3];
 
