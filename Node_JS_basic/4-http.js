acc1929274f9a12c36f16788f620807e510c43ad
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-04 23:44:02 +0200

    Correction name varibale serveur

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,12 +3,12 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-}); 
+app.listen(port, hostname);
+
+module.exports = app;
