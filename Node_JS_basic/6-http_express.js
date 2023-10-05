dccecd8688feb7ea265927bd1003e47618fa6551
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-05 13:57:17 +0200

    Correction eslint

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require('express');
 // Create an instance of the express application
-const app = express()
-const port = 1245
+const app = express();
+const port = 1245;
 
 // root endpoint '/'
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!')
-})
+  res.send('Hello Holberton School!');
+});
 
 app.listen(port);
 
