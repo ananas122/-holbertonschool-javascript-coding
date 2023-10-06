da0866f5a7c3388e6bdba644fc20ed0400d2575a
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-06 11:50:22 +0200

    Correction es lint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -7,23 +7,25 @@
 */
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
     const lines = data.split('\n');
     // Créer un [objet] représentant les étudiants en excluant la ligne 0
-    const students = lines.slice(1).map(line => {
+    const students = lines.slice(1).map((line) => {
       const [firstName, lastName, age, field] = line.split(',');
       // return l'objet etudient en convertissant l age en nb decimal 10
-      return { firstName, lastName, age: parseInt(age, 10), field };
+      return {
+        firstName, lastName, age: parseInt(age, 10), field,
+      };
     });
     // Filtrer les étudiants par domaine d'études (CS ou SWE)
-    const csStudents = students.filter(student => student.field === 'CS');
-    const sweStudents = students.filter(student => student.field === 'SWE');
+    const csStudents = students.filter((student) => student.field === 'CS');
+    const sweStudents = students.filter((student) => student.field === 'SWE');
 
     console.log(`Number of students: ${students.length}`);
-    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.map(student => student.firstName).join(', ')}`);
-    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map(student => student.firstName).join(', ')}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.map((student) => student.firstName).join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.map((student) => student.firstName).join(', ')}`);
   } catch (err) {
     throw new Error('Cannot load the database');
   }
