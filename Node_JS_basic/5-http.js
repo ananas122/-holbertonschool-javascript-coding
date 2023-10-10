886c1697565e7b2e0e62974e4c67481c992c8baf
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-10 10:25:32 +0200

    Correction

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,25 +1,22 @@
-const express = require('express');
+const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
-app.get('/students', async (req, res) => {
-  res.write('This is the list of our students\n');
-  try {
-    const students = await countStudents(process.argv[2]);
-    res.end(students.join('\n'));
-    res.statusCode = 200;
-    res.end();
-  } catch (error) {
-    res.statusCode = 500;
-    res.end(error.message);
+const port = 1245;
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
   }
-});
-
-app.listen(1245);
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.end();
+}).listen(port);
 
 module.exports = app;
