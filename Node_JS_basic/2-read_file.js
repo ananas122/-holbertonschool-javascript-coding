766ea9194947d1a278d3ae621402263453977b85
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-10-10 09:47:02 +0200

    Readfile sync

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,25 +1,18 @@
-/**
-* Compte le nombre d'étudiants par domaine d'études à partir d'un fichier CSV.
-* Analyse un fichier CSV contenant des données étudiantes et affiche
-* un résumé du nombre d'étudiants par domaine.
-* @param {string} path - Chemin du fichier CSV
-* @throws {Error} Si le fichier ne peut pas être lu
-*/
 const fs = require('fs');
 
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.split('\n');
-    // Créer un [objet] représentant les étudiants en excluant la ligne 0
+    const lines = data.split('\n').filter(Boolean);
+
     const students = lines.slice(1).map((line) => {
       const [firstName, lastName, age, field] = line.split(',');
-      // return l'objet etudient en convertissant l age en nb decimal 10
+
       return {
-        firstName, lastName, age: parseInt(age, 10), field,
+        firstName, lastName, age, field,
       };
     });
-    // Filtrer les étudiants par domaine d'études (CS ou SWE)
+
     const csStudents = students.filter((student) => student.field === 'CS');
     const sweStudents = students.filter((student) => student.field === 'SWE');
 
@@ -31,4 +24,4 @@ function countStudents(path) {
   }
 }
 
-module.exports = countStudents;
+module.exports = countStudent
